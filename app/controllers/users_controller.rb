8175545b939648126a4b4ff60afefb4476c35b67
Carl Abelin <carl.abelin@gmail.com>
2011-09-07 20:08:06 +0200

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      @title = "Sign upss"
+      @title = "Sign up"
       @user.password = ""
       @user.password_confirmation = ""
       render 'new'
